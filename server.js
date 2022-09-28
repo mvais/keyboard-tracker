3b54237f557456f85170bf8631e40c557b8f4a61
Bernard Lau <laubernardsy@gmail.com>
2022-09-28 01:24:33 -0400

    Changed port

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 8080;
+const port = 3000;
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
